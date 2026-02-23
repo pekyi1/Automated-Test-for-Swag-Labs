9ed4e2212082909bade72b657affdf2064a75aee
QA Automation Engineer <qa.automation@xyzbank.com>
2026-02-23 09:37:59 +0000

    added screenshots to the allure report

--- src/test/java/utils/ScreenshotListener.java
+++ src/test/java/utils/ScreenshotListener.java
@@ -2,21 +2,28 @@ package utils;
 
 import base.BaseTest;
 import io.qameta.allure.Attachment;
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.api.extension.TestWatcher;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
-public class ScreenshotListener implements TestWatcher {
+public class ScreenshotListener implements AfterTestExecutionCallback {
 
     @Override
-    public void testFailed(ExtensionContext context, Throwable cause) {
-        Object testInstance = context.getRequiredTestInstance();
-        if (testInstance instanceof BaseTest) {
-            WebDriver driver = ((BaseTest) testInstance).getDriver();
-            if (driver != null) {
-                captureScreenshotOnFailure(driver);
+    public void afterTestExecution(ExtensionContext context) throws Exception {
+        boolean testFailed = context.getExecutionException().isPresent();
+        if (testFailed) {
+            Object testInstance = context.getRequiredTestInstance();
+            if (testInstance instanceof BaseTest) {
+                WebDriver driver = ((BaseTest) testInstance).getDriver();
+                if (driver != null) {
+                    try {
+                        captureScreenshotOnFailure(driver);
+                    } catch (Exception e) {
+                        System.err.println("Failed to capture screenshot: " + e.getMessage());
+                    }
+                }
             }
         }
     }
